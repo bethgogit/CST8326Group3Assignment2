47378463211101b4bca59e2bcfcc93dd88acfa85
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-03-30 19:17:00 -0400

    added pug rendering example to server.js

--- Assignmet 2/server.js
+++ Assignmet 2/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
-	res.send("hi");
-});
+app.use(express.static("public"));
 
+app.set("views", "views");
+app.set("view engine", "pug");
+
+app.get("/", function (req, res) {
+   res.render("index");
+});
 
 const PORT = 3000;
 app.listen(PORT,() => {
