9d866a7bf5959159f22991fdf90519477533035b
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-03-31 12:31:42 -0400

    index page now uses a router

--- Assignmet 2/server.js
+++ Assignmet 2/server.js
@@ -6,9 +6,8 @@ app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
-   res.render("index");
-});
+app.use("/", require("./routes/index"));
+
 
 const PORT = 3000;
 app.listen(PORT,() => {
