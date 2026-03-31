0cec3afdc5363287b5c3e8dab9940030407e6721
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-03-31 12:31:42 -0400

    index page now uses a router

--- Assignmet 2/server.js
+++ Assignmet 2/server.js
@@ -6,9 +6,8 @@ app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
-   res.render("index");
-});
+//this line is required for including the router. You only need to change the require()'s specified path.
+app.use("/", require("./routes/index"));
 
 const PORT = 3000;
 app.listen(PORT,() => {
