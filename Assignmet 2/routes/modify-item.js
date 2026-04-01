42dbb4dbcbeebca2a32e13248b93563de35ad6a8
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-04-01 18:31:23 -0400

    Page now indicates if an item is being added or deleted, new items now use /newItem

--- Assignmet 2/routes/modify-item.js
+++ Assignmet 2/routes/modify-item.js
@@ -7,11 +7,12 @@ const Menu = require('../models/Menu');
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 
+router.get("/newItem", async function(req, res) {
+   res.status(200).render("modify-item");
+}) 
+
 router.get("/modify/:name", async function (req, res) {
-   if (req.params.name === "new") {
-      res.status(200).render("modify-item");
-   } else {
-      try {
+   try {
          console.log("loading item with name "+req.params.name)
          const item = await Menu.findOne({ name: req.params.name});
          if (!item) return res.status(404).json({ success: false, message: 'Item not found' });
@@ -29,7 +30,6 @@ router.get("/modify/:name", async function (req, res) {
       } catch (err) {
          res.status(500).json({ success: false, message: err.message });
       }
-   }
 });
 
 function setFields(item, req) {
