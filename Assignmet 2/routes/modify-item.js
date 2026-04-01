acba4bb05f8fc3de1adca5e2acf38fe0e226eaae
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-04-01 16:13:12 -0400

    items can now be posted to the database

--- Assignmet 2/routes/modify-item.js
+++ Assignmet 2/routes/modify-item.js
@@ -8,28 +8,38 @@ router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 
 router.get("/modify/:name", async function (req, res) {
-   try {
-      console.log("loading item with name "+req.params.name)
-      const item = await Menu.findOne({ name: req.params.name});
-      if (!item) return res.status(404).json({ success: false, message: 'Item not found' });
-      console.log(item);
-      //I don't understand why I can't pass the item directly.
-      res.status(200).render("modify-item",{
-            name: item.name,
-            description: item.description,
-            imagePredefined: item.imagePredefined,
-            imageUrl: item.imageUrl,
-            size: item.size,
-            crust: item.crust,
-            toppings: item.toppings,
-      });
-   } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
+   if (req.params.name === "new") {
+      res.status(200).render("modify-item");
+   } else {
+      try {
+         console.log("loading item with name "+req.params.name)
+         const item = await Menu.findOne({ name: req.params.name});
+         if (!item) return res.status(404).json({ success: false, message: 'Item not found' });
+         console.log(item);
+         //I don't understand why I can't pass the item directly.
+         res.status(200).render("modify-item",{
+               name: item.name,
+               description: item.description,
+               imagePredefined: item.imagePredefined,
+               imageUrl: item.imageUrl,
+               size: item.size,
+               crust: item.crust,
+               toppings: item.toppings,
+         });
+      } catch (err) {
+         res.status(500).json({ success: false, message: err.message });
+      }
    }
 });
 
-router.post("/modify", async function (req, res) {
-   //TODO: Missing server side validation
+function setFields(item, req) {
+   item.name = req.body.name;
+   item.description = req.body.description;
+   item.imagePredefined = req.body.imagePredefined;
+   item.imageUrl = req.body.imageURL;
+   item.size = req.body.size;
+   item.crust = req.body.crust;
+
    const TOPPING_KEYWORD = "topping";
    var toppings = [];
    for (key in req.body) {
@@ -37,19 +47,21 @@ router.post("/modify", async function (req, res) {
          toppings.push(key.substring(TOPPING_KEYWORD.length).toLowerCase());
       }
    }
+   item.toppings =  toppings
+}
 
-   const item = new Menu({
-      name: req.body.name,
-    description: req.body.description,
-    imagePredefined: req.body.imagePredefined,
-    imageUrl: req.body.imageURL,
-    size: req.body.size,
-    crust: req.body.crust,
-    toppings: toppings
-   });
-   await item.save()
-   //TODO: redirect to an appropriate page
-   res.redirect("/modify");
+router.post("/modify", async function (req, res) {
+   try {
+      var item = await Menu.findOne({ name: req.params.name});
+      if (item === null) item = new Menu()
+      console.log(item);
+      setFields(item,req);
+      item.save()
+      //TODO: should probably redirect instead
+      res.status(200).render("modify-item");
+   } catch (err) {
+      res.status(500).json({ success: false, message: err.message });
+   }
 });
 
 
