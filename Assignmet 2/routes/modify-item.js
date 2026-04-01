b676a19cfe7fcc603ed4cdfdb5e58942cff14a06
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-03-31 20:52:16 -0400

    server now processes post

--- Assignmet 2/routes/modify-item.js
+++ Assignmet 2/routes/modify-item.js
@@ -1,9 +1,17 @@
 var express = require('express');
 const router = express.Router()
 
-//example middleware function for a router.
+router.use(express.urlencoded({ extended: false }));
+
 router.get("/modify", function (req, res) {
+   //TODO: add parameter object containing item info
    res.render("modify-item");
+});
+
+router.post("/modify", function (req, res) {
+   console.log(req.body);
+   //TODO: redirect to an appropriate page
+   res.redirect("/modify");
 });
 
 module.exports = router
