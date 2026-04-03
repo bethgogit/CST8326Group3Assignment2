b58e7f5a969ea443122314bdd61d48488ab334d6
Benjamin Theilmann Gohr <thei0019@algonquinlive.com>
2026-04-02 22:13:16 -0400

    Menu now lists all items in menu collection

--- Assignmet 2/routes/menu.js
+++ Assignmet 2/routes/menu.js
@@ -1,8 +1,24 @@
 var express = require('express');
 const router = express.Router()
 
-router.get("/menu", function (req, res) {
-   res.render("menu");
+const mongoose = require('mongoose');
+const Menu = require('../models/Menu');
+
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+
+router.get("/menu", async function (req, res) {
+    var items;
+    try {
+        items = await Menu.find({});
+        if (!items) return res.status(404).json({ success: false, message: 'Item not found' });
+        res.status(200)
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+    res.render("menu",{
+        list: items
+    });
 });
 
 module.exports = router
